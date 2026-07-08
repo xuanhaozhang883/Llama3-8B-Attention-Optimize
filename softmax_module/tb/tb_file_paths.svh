71991101934d26da622778c9e7e54e579d0db50a
lhming <liuhemingnb@126.com>
2026-07-08 16:16:27 +0800

    softmax模块

---
+++ softmax_module/tb/tb_file_paths.svh
@@ -0,0 +1,8 @@
+`define INPUT_HEX_FILE        "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/data/input_scores_bf16.mem"
+`define MASK_HEX_FILE         "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/data/input_masks.mem"
+`define EXPECTED_BF16_FILE    "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/data/expected_probs_bf16.mem"
+`define EXPECTED_FP32_FILE    "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/data/expected_probs_fp32.mem"
+`define RESULT_CSV_FILE       "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/results/softmax_results.csv"
+`define ROW_SUMMARY_CSV_FILE  "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/results/softmax_row_summary.csv"
+`define EXP_LUT_FILE          "C:/Users/liuhe/Desktop/softmax_golden_test/softmax_golden_test/rtl/exp_lut_q15.mem"
+`define SOFTMAX_TOL_ABS       0.0025

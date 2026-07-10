4bf88a33a1f5faab7286f5b1e0acd5bbbdff0555
4520-Ui <why15167689552@outlook.com>
2026-07-10 09:58:17 +0800

    Remove generated Vitis files from attention mask PR

--- attention_mask_module/TEST_v1/attention_mask/hls/.autopilot/db/coregen/control.slave.tcl
+++
@@ -1,4 +0,0 @@
-dict set slaves control {ports {raw_scores {type i_ap_none width 64} masked_scores {type i_ap_none width 64} q_heads {type i_ap_none width 32} seq_len {type i_ap_none width 32} causal {type i_ap_none width 1} mask_value {type i_ap_none width 16} ap_start {type ap_ctrl width 1} ap_done {type ap_ctrl width 1} ap_ready {type ap_ctrl width 1} ap_idle {type ap_ctrl width 1}} mems {} has_ctrl 1}
-set datawidth 32
-set addrwidth 64
-set intr_clr_mode TOW
--- attention_mask_module/TEST_v1/attention_mask/hls/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,7 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "attention_mask", "RefName" : "attention_mask","ID" : "0","Type" : "sequential",
-"SubInsts" : [
-	{"Name" : "grp_attention_mask_Pipeline_QH_LOOP_QT_LOOP_KT_LOOP_fu_116", "RefName" : "attention_mask_Pipeline_QH_LOOP_QT_LOOP_KT_LOOP","ID" : "1","Type" : "sequential",
-		"SubLoops" : [
-		{"Name" : "QH_LOOP_QT_LOOP_KT_LOOP","RefName" : "QH_LOOP_QT_LOOP_KT_LOOP","ID" : "2","Type" : "pipeline"},]},]
-}]}

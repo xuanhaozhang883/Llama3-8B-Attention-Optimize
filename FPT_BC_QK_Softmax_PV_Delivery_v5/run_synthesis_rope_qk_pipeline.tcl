f264c0c7c60e1453370071b5585b5d96dda58429
4520-Ui <why15167689552@outlook.com>
2026-07-20 20:40:13 +0800

    integrate RoPE with QK pipeline

---
+++ FPT_BC_QK_Softmax_PV_Delivery_v5/run_synthesis_rope_qk_pipeline.tcl
@@ -0,0 +1,1 @@
+source [file join [file dirname [info script]] scripts run_synthesis_rope_qk_pipeline.tcl]
---
+++ FPT_BC_QK_Softmax_PV_Delivery_v5/run_vivado_rope_qk_small.tcl
@@ -0,0 +1,1 @@
+source [file join [file dirname [info script]] scripts run_vivado_rope_qk_small.tcl]
